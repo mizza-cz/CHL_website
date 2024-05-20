4b25ebe96fbbb8070358f66070ec741bdd1fe8a9
mizza-cz <mizza.cz666@gmail.com>
2024-05-20 10:46:03 +0200

    add test

---
+++ src/components/stories-section/stories-section.js
@@ -0,0 +1,27 @@
+$('.stories-section__slider').slick({
+  centerMode: true,
+  centerPadding: '240px',
+  slidesToShow: 3,
+  prevArrow:
+    '<button class="slick-arrow slick-prev"><img src="/images/ico/prev.svg" loading="lazy" alt="" /></button>',
+  nextArrow:
+    '<button class="slick-arrow slick-next"><img src="/images/ico/next.svg" loading="lazy" alt="" /></button>',
+  responsive: [
+    {
+      breakpoint: 1320,
+      settings: {
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 740,
+      settings: {
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
