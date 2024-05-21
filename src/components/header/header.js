ecf71c3f0d5ccb74581d646f9fe891b9b6d60a25
mizza-cz <mizza.cz666@gmail.com>
2024-05-21 14:11:40 +0200

    add content-3

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,27 @@
 headerNavOpenerClick();
 function headerNavOpenerClick() {
-  const bodyEl = document.querySelector("body");
-  const headerNavOpener = document.querySelector(".js-header__opener");
+  const bodyEl = document.querySelector('body');
+  const headerNavOpener = document.querySelector('.js-header__opener');
   if (!bodyEl || !headerNavOpener) {
     return;
   }
-  headerNavOpener.addEventListener("click", function () {
-    if (!this.classList.contains("is-open")) {
-      bodyEl.classList.add("is-nav-open");
-      this.classList.add("is-open");
+  headerNavOpener.addEventListener('click', function () {
+    if (!this.classList.contains('is-open')) {
+      bodyEl.classList.add('is-nav-open');
+      this.classList.add('is-open');
     } else {
-      bodyEl.classList.remove("is-nav-open");
-      this.classList.remove("is-open");
+      bodyEl.classList.remove('is-nav-open');
+      this.classList.remove('is-open');
     }
   });
 }
+
+document.querySelectorAll('.nav-link').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
